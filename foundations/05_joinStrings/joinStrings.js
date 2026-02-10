cf390d4d6abcf45b45369b05cdbad23a31355923
Charlie <sonthayaphuree@gmail.com>
2026-02-10 15:29:50 +0700

    Solve first 5 exercise in foundation topic

--- foundations/05_joinStrings/joinStrings.js
+++ foundations/05_joinStrings/joinStrings.js
@@ -3,14 +3,18 @@
  Last name: Stevenson
  This year: 1965
  Birth year: 1947
-
  The greeting should read: "Hello! My name is Carlos Stevenson and I am 18 years old."
 */
 
 // Add your code right below, good luck!
+let firstName = "Carlos";
+let lastName = "Stevenson";
+let thisYear = 1965;
+const birthYear = 1947;
+let age = thisYear-birthYear;
+let fullName = firstName +" "+ lastName;
 
-
-
+let greeting = "Hello! My name is "+fullName + " and I am " +age +" years old."
 
 
 // Do not change this
