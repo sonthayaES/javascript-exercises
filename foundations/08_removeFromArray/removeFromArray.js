5d151d8634f58f2d1361819a9eb6e7aff0edf3b1
Charlie <sonthayaphuree@gmail.com>
2026-02-22 01:00:30 +0700

    Solved exercise 08 of Foundations

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...itemsToRemove) {
+        let remainArr = array
+        .filter((item) => !itemsToRemove.includes(item));
+        return remainArr;
 };
 
 // Do not edit below this line
