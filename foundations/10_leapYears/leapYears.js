10179375b79a5f9c49cc62efef07fb4fca278757
Charlie <sonthayaphuree@gmail.com>
2026-02-22 04:07:25 +0700

    Solved exercise 10 of foundations

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,6 +1,28 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if((normalLeapYear(year)) || (centuryLeap(year))){
+        return true;
+    }
+    else{
+        return false;
+    }
+    
 };
+const normalLeapYear = (year) =>{
+    if(year % 4 === 0 && year % 100 !== 0){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+const centuryLeap = (year) =>{
+    if(year % 400 === 0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
 
 // Do not edit below this line
 module.exports = leapYears;
