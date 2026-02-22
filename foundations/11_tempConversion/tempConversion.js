a477d48636135f6d724110aa28aafc433362893b
Charlie <sonthayaphuree@gmail.com>
2026-02-22 19:09:33 +0700

    Solved exercise 11 of foundations

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
-};
+const convertToCelsius = function(temp) {
+  let celsiusConverted = (temp - 32) * 5 / 9;
+  celsiusConverted = Math.round(celsiusConverted * 10) / 10;
+  return celsiusConverted;
+};   
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let fahrenheitConverted = (temp * 9 / 5) + 32;
+  fahrenheitConverted = Math.round(fahrenheitConverted * 10) / 10;
+  return fahrenheitConverted;
 };
 
 // Do not edit below this line
