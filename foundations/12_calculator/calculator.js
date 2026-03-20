7455641d8aff329c56b589d2d70e832382573e8e
Charlie <sonthayaphuree@gmail.com>
2026-03-20 16:15:20 +0700

    solve exercise 12th

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((total, currentNumber) =>{
+    return total + currentNumber;
+  },0);
 };
 
-const multiply = function() {
-
+const multiply = function(multipliers) {
+  return multipliers.reduce((total, currentNumber) =>{
+    return total * currentNumber;
+  });
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(factorialNum) {
+  if(factorialNum == 0){
+    return 1;
+  }
+  let total = 1;
+  for(let i = 1; i <= factorialNum; i++ ){
+    total = total * i;
+  }
+
+  return total;
 };
 
 // Do not edit below this line
