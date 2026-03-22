ade30c2efde2601b88ce0265684592a2c1226f65
Charlie <sonthayaphuree@gmail.com>
2026-03-23 01:56:24 +0700

    solve exercise 15 select value out of objects from array

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(booksArr) {
+    const titles = booksArr.map(bookTitle => bookTitle.title);
+    return titles;
 };
 
 // Do not edit below this line
