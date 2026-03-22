bc39c099116e3c93d045dbb3fc1e5564e6dcffa9
Charlie <sonthayaphuree@gmail.com>
2026-03-23 00:36:58 +0700

    solve exercise 13 palindrome

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (words) {
+    let lowerWord = words.toLowerCase();
+    const pureString = lowerWord.replace(/[^a-z0-9]/g, '')
+    let reverseString = pureString.split('').reverse().join('');
+    if(reverseString == pureString){
+        return true;
+    }else{
+        return false;
+    }
 };
 
 // Do not edit below this line
