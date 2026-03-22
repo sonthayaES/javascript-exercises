530dfb673517146f85dd7806dc5be6703b5e1681
Charlie <sonthayaphuree@gmail.com>
2026-03-23 01:32:09 +0700

    solve exercise 14 fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let fibo = parseInt(num);
+    if(fibo < 0){
+        return "OOPS"
+    }
+    if(fibo === 0){
+        return 0;
+    }
 
-};
+    let fiboArr = [0,1];
+    for(let i = 2; i <= fibo; i++){
+        let nextNum = fiboArr[i - 1] + fiboArr[i -2];
+
+        fiboArr.push(nextNum);
+
+    }
+    return fiboArr[fibo];
+}
+    
 
 // Do not edit below this line
 module.exports = fibonacci;
