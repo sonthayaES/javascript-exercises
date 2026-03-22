05b043a9f66c54943cde719afb259dd9febb1f0e
Charlie <sonthayaphuree@gmail.com>
2026-03-23 02:43:45 +0700

    solve exercise 16 find oldest by using reduce method

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,29 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleArr) {
+    const oldestPerson = peopleArr.reduce((champion, challenger) =>{
+        const currentYear = new Date().getFullYear();
+        let championAge;
+        if(champion.yearOfDeath){
+            championAge = champion.yearOfDeath - champion.yearOfBirth
+        }
+        else{
+            championAge = currentYear - champion.yearOfBirth;
+        }
+        
+        let challengerAge;
+        if(challenger.yearOfDeath){
+            challengerAge = challenger.yearOfDeath - challenger.yearOfBirth
+        }
+        else{
+            challengerAge = currentYear - challengerAge.yearOfBirth;
+        }
 
+        if(championAge > challengerAge){
+            return champion;
+        }else{
+            return challenger;
+        }
+    })
+    return oldestPerson;
 };
 
 // Do not edit below this line
